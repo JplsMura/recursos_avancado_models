19c680f30ac7109ab4888b70111a802792374650
JplsMura <joaopedrolima.s@hotmail.com>
2020-03-22 20:34:46 -0300

    Gestão de Relacionamento

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Address;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -53,7 +54,7 @@ class UserController extends Controller
         echo "Nome do usuário: {$user->name}<br>";
         echo "Email do usuário: {$user->email}<br>";
 
-        $userAdress = $user->adressDelivery()->get()->first();
+        $userAdress = $user->addressDelivery()->get()->first();
 
         if($userAdress){
 
@@ -63,6 +64,92 @@ class UserController extends Controller
             echo "Complemento: {$userAdress->complement}, {$userAdress->zipcode}<br>";
             echo "Cidade/Estado: {$userAdress->city}/{$userAdress->state}";
         }
+
+        /*Criação de endereço de usuário a partir do próprio usuario, passando o model de Delivery e chamando
+        obj e passando dentro do save*/
+//        $address = new Address([
+//            'address' => 'Rua dos Bobos',
+//            'number' => '0',
+//            'complement' => 'Apto 123',
+//            'zipcode' => '88000-000',
+//            'city' => 'São Paulo',
+//            'state' => 'SP'
+//        ]);
+//
+//        $user->addressDelivery()->save($address);
+
+        /*Salvando endereço atacando os obj diretamente e chamando o método save*/
+//        $address = new Address();
+//        $address ->address = "Rua dos bobos 1";
+//        $address ->number = "02";
+//        $address ->complement = "Apto 000";
+//        $address ->zipcode = "09900-000";
+//        $address ->city = "Belo Horizonte";
+//        $address ->state = "MG";
+//
+//        $user->addressDelivery()->save($address);
+
+        /*Criando endereço a partir do usuário com saveMany para criar mais de um endereço para o mesmo usuário*/
+//        $addressTeste = new Address([
+//            'address' => 'Rua dos Bobos 111',
+//            'number' => '0',
+//            'complement' => 'Apto 123',
+//            'zipcode' => '88000-000',
+//            'city' => 'São Paulo',
+//            'state' => 'SP'
+//        ]);
+//
+//        $address = new Address();
+//        $address ->address = "Rua dos bobos 222";
+//        $address ->number = "02";
+//        $address ->complement = "Apto 000";
+//        $address ->zipcode = "09900-000";
+//        $address ->city = "Belo Horizonte";
+//        $address ->state = "MG";
+//
+//        $user->addressDelivery()->saveMany([$addressTeste, $address]);
+
+        /*OBS: Obrigatoriamente quando eu estou salvando um endereço usando o save(), obrigatoriamente o mesmo deve
+        estar passando por um modelo, como os exemplos acima, que nos casos acima é o Address*/
+
+
+
+
+
+        /*Criando endereço a partir do create, a passando dos parametrôs não necessitam passar por um modelo porém,
+        a aplicação se torna um pouco menos segura ou sucetivel a erros por as regras existentes dentro dos models
+        não iram afetar o create*/
+
+//        $user->addresDelivery()->create([
+//            'address' => 'Rua Teste',
+//            'number' => '25',
+//            'complement' => 'Apto 123',
+//            'zipcode' => '07700-000',
+//            'city' => 'Rio de Janeiro',
+//            'state' => 'RJ'
+//        ]);
+
+        /*Assim como o create acima tem o createMany, para passar mais de um parametro*/
+//        $user->addressDelivery()->createMany([[
+//            'address' => 'Rua Teste 0102',
+//            'number' => '0102',
+//            'complement' => 'Apto 123',
+//            'zipcode' => '06600-000',
+//            'city' => 'Rio de Janeiro',
+//            'state' => 'RJ'
+//        ], [
+//            'address' => 'Rua Teste 0304',
+//            'number' => '0304',
+//            'complement' => 'Apto 123',
+//            'zipcode' => '05500-000',
+//            'city' => 'Rio de Janeiro',
+//            'state' => 'RJ'
+//        ]]);
+
+        /*Evitar o uso do witch pois ele tráz uma carga gigantesca ao banco de dados por a cada usuário buscado
+        ele traz o relacionamento do mesmo e pode diminuir a performance do banco de dados*/
+//        $users = User::with(addressDelivery);
+//        dd($users);
 
     }
 
